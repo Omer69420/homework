2547e1189ee108a37d857947e61d8ecf400fb5db
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-22 23:35:45 +0100

    Något fel med min github

---
+++ 46/2/script.js
@@ -0,0 +1,7 @@
+let animal = {
+    species: "Cat",
+    age: 1,
+    name: "Pamuk"
+};
+
+console.log(animal);
---
+++ 46/3/script.js
@@ -0,0 +1,19 @@
+let animal = {
+    species: "Cat",
+    age: 1,
+    name: "Pamuk"
+};
+
+console.log(animal);
+
+
+animal.age ++;
+
+
+console.log(animal);
+
+
+animal.age += 1;
+
+
+console.log(animal);
